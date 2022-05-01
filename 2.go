108eb4d11dabac600928b66cc26ac37d92228c43
Salamat Tulkibayev <fxmaster@tuta.io>
2022-05-01 23:59:37 +0600

    added 2nd task solution

---
+++ 2.go
@@ -0,0 +1,13 @@
+package main
+
+
+func missingNumber(nums []int) int {
+    var s1, s2, i int
+    
+    for i < len(nums) {
+        s1 += i
+        s2 += nums[i]
+        i++
+    }
+    return s1-s2+i
+}
