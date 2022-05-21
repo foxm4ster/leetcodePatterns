9d3be4ed3fbc880aa4ec458c44624128eeeb1cc9
Salamat Tulkibayev <fxmaster@tuta.io>
2022-05-21 15:20:59 +0600

    added 12-pattern

---
+++ 12.go
@@ -0,0 +1,24 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+	var result ListNode
+	result.Next = head
+
+	current := &result
+	for current.Next != nil {
+		if current.Next.Val == val {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+
+	return result.Next
+}
